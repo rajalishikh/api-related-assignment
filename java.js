e6eb526f74c83003bab50238adf486870a81d6db
rajalishikh <nahidujjamanraj@gmail.com>
2023-09-06 08:07:22 +0600

    add my details section

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-const useData = async (searchText) => {
+const useData = async (searchText='12') => {
   const loadData = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const response = await loadData.json();
   // console.log(response.data);
@@ -35,28 +35,66 @@ const displayData = (data) => {
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>${phone.slug}</p>
             <div class="card-actions justify-center">
-              <button class="btn btn-primary">Buy Now</button>
+              <button onclick="showDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
 
           </div>`
     // append the child 
     findDivContainer.appendChild(createTheDiv);
     
+    
   });
+  addLoading(false);
+  
+  
 }
 
 // find the search item 
 const searchField = () => {
   const searchField = document.getElementById('search-field');
   const searchText1 = searchField.value;
+  addLoading(true);
   console.log(searchText1)
   useData(searchText1);
   
   
 }
-const addMoreButton = () => {
-  const my2ndSearchBar = document.getElementById('my-2nd-search');
-  const searchText2 = my2ndSearchBar.value;
-  useData(searchText2);
+// // recap search bar
+// const addMoreButton = () => {
+//   const my2ndSearchBar = document.getElementById('my-2nd-search');
+//   const searchText2 = my2ndSearchBar.value;
+//   addLoading(true);
+//   useData(searchText2);
+// }
+const addLoading = (isLoading) => {
+  const findLoadingBar = document.getElementById('loading-bar');
+  if (isLoading) {
+    findLoadingBar.classList.remove('hidden');
+  }
+  else {
+    findLoadingBar.classList.add('hidden');
+  }
 }
-useData()
+const showDetails = async (id) => {
+  console.log('show details', id)
+  // find the data use dynamic
+  const response =await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await response.json();
+  // console.log(data);
+  showPhoneDetails(data);
+  
+}
+
+const showPhoneDetails = (phone) => {
+  my_modal_5.showModal()
+  console.log(phone);
+  const image = document.getElementById('image-details');
+  image.innerHTML = `
+  <img src="${phone?.data?.image}" alt="" >
+  <p class="text-center">${phone?.data?.brand}</p>
+  <P class="text-center">${phone?.data?.mainFeatures?.storage}</P>
+  `
+  
+}
+
+useData();
